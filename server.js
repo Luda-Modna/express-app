5b47f19e2fc2be78de1b7b651d2ccc70c602a00a
Luda Modna <ludmilamodna3@gmail.com>
2025-08-22 18:39:37 +0300

    Refactor server

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const http = require('http');
+const app = require('./app.js');
 
 const PORT = process.env.PORT || 3000;
 
-const httpServer = http.createServer((reg, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('result');
-});
+const httpServer = http.createServer(app);
 
 httpServer.listen(PORT, () => {
   console.log(`Server listen http://localhost:${PORT}`);
